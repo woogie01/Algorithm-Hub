12f72e7197d6f262442afe7087fe086528af94fa
Hyukwon <113490741+woogie01@users.noreply.github.com>
2025-02-12 17:03:33 +0900

    Delete 0125-valid-palindrome directory

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public boolean isPalindrome(String s) {
-        // 정규식 활용
-        String filter = s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
-        String reverse = new StringBuilder(filter).reverse().toString();
-
-        return filter.equals(reverse);
-    }
-}
