6eeadf9fc2a497f782fe39281c3e123d8b8bb018
Hyukwon <113490741+woogie01@users.noreply.github.com>
2025-03-02 21:24:57 +0900

    Time: 30 ms (67.8%), Space: 51.5 MB (59.4%) - LeetHub

---
+++ LeetCode/Medium/0015-3sum/0015-3sum.java
@@ -0,0 +1,34 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        int left, right, sum;
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+            
+            left = i + 1;
+            right = nums.length - 1;
+            while (left < right) {
+                sum = nums[i] + nums[left] + nums[right];
+                if (sum < 0) {
+                    left++;
+                } else if (sum > 0) {
+                    right--;
+                } else {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+
+                    // 중복 숫자 처리
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+                    
+                    // 정답 범위 제외
+                    left++;
+                    right--;
+                }
+            }
+        }
+        return result;
+    }
+}
