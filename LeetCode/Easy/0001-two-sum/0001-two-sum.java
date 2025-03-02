299486b6ba70934593ef340fa99ca24ad9ce3adc
Hyukwon <113490741+woogie01@users.noreply.github.com>
2025-03-02 20:19:32 +0900

    Time: 2 ms (98.84%), Space: 45.1 MB (29.15%) - LeetHub

---
+++ LeetCode/Easy/0001-two-sum/0001-two-sum.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numsMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (numsMap.containsKey(target - nums[i])) {
+                return new int[]{numsMap.get(target - nums[i]), i};
+            }
+            // 값으로 인덱스 찾기
+            numsMap.put(nums[i], i);
+        }
+        return null;
+    }
+}
