97dfb115a6ddf3d1a35f6b96679585cd7ddbc836
Hyukwon <113490741+woogie01@users.noreply.github.com>
2025-03-02 21:31:58 +0900

    Time: 841 ms (13.63%), Space: 52.2 MB (32.78%) - LeetHub

--- LeetCode/Medium/0015-3sum/0015-3sum.java
+++ LeetCode/Medium/0015-3sum/0015-3sum.java
@@ -1,33 +1,24 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
+        Set<List<Integer>> resultSet = new HashSet<>();
         Arrays.sort(nums);
 
         for (int i = 0; i < nums.length - 2; i++) {
-            /** 중복 숫자 처리 조건 */
-            if (i == 0 || (i > 0 && nums[i] != nums[i - 1])) { 
-                int left = i + 1, right = nums.length - 1, target = -nums[i];
-                
-                while (left < right) {
-                    int sum = nums[left] + nums[right];
-                    if (sum == target) {
-                        result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+            int left = i + 1, right = nums.length - 1, target = -nums[i];
 
-                        /** 중복 숫자 처리 */
-                        while (left < right && nums[left] == nums[left + 1]) left++;
-                        while (left < right && nums[right] == nums[right - 1]) right--;
-                        
-                        /** 정답 범위에서 제거 */
-                        left++;
-                        right--;
-                    } else if (sum < target) {
-                        left++;
-                    } else {
-                        right--;
-                    }
+            while (left < right) {
+                int sum = nums[left] + nums[right];
+                if (sum == target) {
+                    resultSet.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    left++;
+                    right--;
+                } else if (sum < target) {
+                    left++;
+                } else {
+                    right--;
                 }
             }
         }
-        return result;
+        return new ArrayList<>(resultSet);
     }
 }
