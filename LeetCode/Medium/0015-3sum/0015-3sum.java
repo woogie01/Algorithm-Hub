2f744a2fd45d0a3db7f0fcef821f1bf7ba389b5d
Hyukwon <113490741+woogie01@users.noreply.github.com>
2025-03-02 21:30:55 +0900

    Time: 31 ms (58.47%), Space: 51.4 MB (71.45%) - LeetHub

--- LeetCode/Medium/0015-3sum/0015-3sum.java
+++ LeetCode/Medium/0015-3sum/0015-3sum.java
@@ -1,31 +1,30 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        int left, right, sum;
         List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(nums);
 
         for (int i = 0; i < nums.length - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1])
-                continue;
-            
-            left = i + 1;
-            right = nums.length - 1;
-            while (left < right) {
-                sum = nums[i] + nums[left] + nums[right];
-                if (sum < 0) {
-                    left++;
-                } else if (sum > 0) {
-                    right--;
-                } else {
-                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+            /** 중복 숫자 처리 조건 */
+            if (i == 0 || (i > 0 && nums[i] != nums[i - 1])) { 
+                int left = i + 1, right = nums.length - 1, target = -nums[i];
+                
+                while (left < right) {
+                    int sum = nums[left] + nums[right];
+                    if (sum == target) {
+                        result.add(Arrays.asList(nums[i], nums[left], nums[right]));
 
-                    // 중복 숫자 처리
-                    while (left < right && nums[left] == nums[left + 1]) left++;
-                    while (left < right && nums[right] == nums[right - 1]) right--;
-                    
-                    // 정답 범위 제외
-                    left++;
-                    right--;
+                        /** 중복 숫자 처리 */
+                        while (left < right && nums[left] == nums[left + 1]) left++;
+                        while (left < right && nums[right] == nums[right - 1]) right--;
+                        
+                        /** 정답 범위에서 제거 */
+                        left++;
+                        right--;
+                    } else if (sum < target) {
+                        left++;
+                    } else {
+                        right--;
+                    }
                 }
             }
         }
