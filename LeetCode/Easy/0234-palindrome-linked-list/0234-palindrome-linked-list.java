95e29cd5ccb9e5c1c5b05f3019d3d04ecea8635e
Hyukwon <113490741+woogie01@users.noreply.github.com>
2025-03-14 12:58:35 +0900

    Time: 3 ms (99.79%), Space: 68.6 MB (37.54%) - LeetHub

--- LeetCode/Easy/0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ LeetCode/Easy/0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -11,17 +11,32 @@
 
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        Deque<Integer> deque = new LinkedList<>();
-        ListNode node = head;
-        while (node != null) {
-            deque.add(node.val);
-            node = node.next;
+        ListNode fast = head, slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
         }
 
-        while (!deque.isEmpty() && deque.size() > 1) {
-            if (deque.pollFirst() != deque.pollLast()) {
+        /** 홀수 예외 처리 */
+        if (fast != null) {
+            slow = slow.next;
+        }
+
+        /** 중간 부분부터 체크 : 역순으로 교환 */
+        ListNode rev = null;
+        while (slow != null) {
+            ListNode next = slow.next;
+            slow.next = rev;
+            rev = slow;
+            slow = next;
+        }
+        
+        while (rev != null) {
+            if (rev.val != head.val) {
                 return false;
             }
+            rev = rev.next;
+            head = head.next;
         }
         return true;
     }
