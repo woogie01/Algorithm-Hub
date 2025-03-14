b3f8212d76a5ce7126a28784693d65a9a0cb2ae8
Hyukwon <113490741+woogie01@users.noreply.github.com>
2025-03-14 17:23:04 +0900

    Time: 0 ms (100%), Space: 41.3 MB (67.1%) - LeetHub

---
+++ LeetCode/Medium/0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        // 임시 노드 
+        ListNode node = new ListNode(0);
+        ListNode root = node;
+
+        node.next = head;
+        while (node.next != null && node.next.next != null) {
+            
+            // node(=first), mid, end
+            ListNode mid = node.next;
+            ListNode end = node.next.next;
+
+            // Swap
+            mid.next = end.next;
+            node.next= end;
+            node.next.next = mid;
+
+            // 두 칸 이동
+            node = node.next.next;
+        }
+        // 임시 노드는 제외
+        return root.next;
+    }
+}
