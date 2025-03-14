bc4b7a14a14351db310c3a1a8a72c5bf4dc41516
Hyukwon <113490741+woogie01@users.noreply.github.com>
2025-03-14 17:08:30 +0900

    Time: 1 ms (100%), Space: 44.3 MB (92.49%) - LeetHub

--- LeetCode/Medium/0002-add-two-numbers/0002-add-two-numbers.java
+++ LeetCode/Medium/0002-add-two-numbers/0002-add-two-numbers.java
@@ -1,5 +1,3 @@
-import java.math.BigInteger;
-
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -13,33 +11,33 @@ import java.math.BigInteger;
 
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-       // 초기 계산을 위한 임시 노드
-       ListNode node = new ListNode(0);
-       ListNode root = node;
+        // 초기 계산을 위한 임시 노드
+        ListNode node = new ListNode(0);
+        ListNode root = node;
 
-       int sum, carry = 0, remainder;
-       while (l1 != null || l2 != null || carry != 0) {
-        sum = 0;
-        // 첫 번째 연결 리스트 계산
-        if (l1 != null) {
-            sum += l1.val;
-            l1 = l1.next;
+        int sum, carry = 0, remainder;
+        while (l1 != null || l2 != null || carry != 0) {
+            sum = 0;
+            // 첫 번째 연결 리스트 계산
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            // 두 번째 연결 리스트 계산
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+
+            // 노드에 남길 값 
+            remainder = (sum + carry) % 10;
+            carry = (sum + carry) / 10;
+
+            // 뒤집어서 노드 연결
+            node.next = new ListNode(remainder);
+            node = node.next;
         }
-        // 두 번째 연결 리스트 계산
-           if (l2 != null) {
-            sum += l2.val;
-            l2 = l2.next;
-        }
-
-        // 노드에 남길 값 
-        remainder = (sum + carry) % 10;
-        carry = (sum + carry) / 10;
-
-        // 뒤집어서 노드 연결
-        node.next = new ListNode(remainder);
-        node = node.next;
-       }
-       // 첫 번째 노드는 제외하고 반환
-       return root.next;
+        // 첫 번째 노드는 제외하고 반환
+        return root.next;
     }
 }
