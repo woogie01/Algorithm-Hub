d7d2746efd90e8a750703329a1c00289d6f9b1b3
Hyukwon <113490741+woogie01@users.noreply.github.com>
2025-04-05 17:10:49 +0900

    Time: 0 ms (100%), Space: 41.1 MB (91.45%) - LeetHub

---
+++ LeetCode/Easy/0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
@@ -0,0 +1,22 @@
+ public class MyStack {
+        Queue<Integer> queue = new LinkedList<>();
+
+        public void push(int x) {
+            queue.add(x);
+            for (int i = 0; i < queue.size() - 1; i++) {
+                queue.add(queue.remove());
+            }
+        }
+
+        public int pop() {
+            return queue.remove();
+        }
+
+        public int top() {
+            return queue.peek();
+        }
+
+        public boolean empty() {
+            return queue.size() == 0;
+        }
+    }
