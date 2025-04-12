b35a32d3a634c4d7f3956b539df3c9b23db6ac75
Hyukwon <113490741+woogie01@users.noreply.github.com>
2025-04-12 14:49:40 +0900

    [Gold V] Title: 탑, Time: 796 ms, Memory: 91052 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -19,36 +19,28 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        Deque<Tower> stack = new ArrayDeque<>();
         int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
+
+        Deque<Tower> stack = new ArrayDeque<>();
         StringBuilder result = new StringBuilder();
 
         for (int i = 1; i <= N; i++) {
             int newHeight = Integer.parseInt(st.nextToken());
 
-            if (stack.isEmpty()) { // 초기 1회 시행
-                result.append("0 ");
-                stack.push(new Tower(i, newHeight));
-            } else {
-                while (true) { 
-                    if (stack.isEmpty()) { // 순회 종료 조건(1)
-                        result.append("0 ");
-                        stack.push(new Tower(i, newHeight));
-                        break;
-                    }
-
-                    Tower tower = stack.peek();
-                    if (tower.height > newHeight) { // 순회 종료 조건(2)
-                        result.append(tower.num).append(" ");
-                        stack.push(new Tower(i, newHeight));
-                        break;
-                    } else {
-                        stack.pop();
-                    }
-                }
+            // 뒤로 순회하면서 검사
+            while (!stack.isEmpty() && stack.peek().height <= newHeight) {
+                stack.pop();
             }
+
+            if (stack.isEmpty()) {
+                result.append("0 ");
+            } else {
+                result.append(stack.peek().num).append(" ");
+            }
+            stack.push(new Tower(i, newHeight));
         }
+
         bw.write(result.toString());
         bw.flush();
         bw.close();
