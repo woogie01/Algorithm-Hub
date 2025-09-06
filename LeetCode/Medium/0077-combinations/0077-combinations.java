d23a1dc6d8f9b2c5c528960a3b69956aab4f8ef4
Hyukwon <113490741+woogie01@users.noreply.github.com>
2025-09-06 14:50:58 +0900

    Time: 28 ms (11.44%), Space: 91.8 MB (98.31%) - LeetHub

---
+++ LeetCode/Medium/0077-combinations/0077-combinations.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> results = new ArrayList<>();
+
+        dfs(results, new LinkedList<>(), n, 1, k);
+        return results;
+    }
+
+    private void dfs(List<List<Integer>> results, LinkedList<Integer> elements, int n, int start, int k) {
+        if (k == 0) {
+            results.add(new ArrayList<>(elements));
+            return;
+        }
+
+        for (int i = start; i <= n; i++) {
+            elements.add(i);
+            dfs(results, elements, n, i + 1, k - 1);
+            elements.removeLast();
+        }
+    }
+}
